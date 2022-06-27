dae47eff2b92705b12c77e13993364795f5782c6
rahmatsyifana12 <adean435@gmail.com>
2022-06-27 23:41:12 +0700

    Add validate middleware for register

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { register } = require('../controllers/auth.controller');
+const { validate } = require('../middlewares/validate.middleware');
+const { newUserSchema } = require('../validations/auth.validate');
 
 const router = Router();
 
-router.post('/api/v1/auth/register', register);
+router.post('/api/v1/auth/register', validate(newUserSchema), register);
 
 module.exports = router;
