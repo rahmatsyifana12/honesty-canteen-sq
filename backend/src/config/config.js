593977783bb231211317d8def2c1e73717f87de5
rahmatsyifana12 <adean435@gmail.com>
2022-06-27 23:46:30 +0700

    Add projects config

---
+++ backend/src/config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { env } = process;
+
+const config = {
+    hashRounds: 10,
+    port: env.LOCAL_PORT,
+    jwt: {
+        accessSecret: env.ACCESS_TOKEN_SECRET,
+        accessExpire: '1d'
+    }
+};
+
+module.exports = config;
