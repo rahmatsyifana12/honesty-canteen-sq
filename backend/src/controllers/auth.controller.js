0c7ca3cfe5187391de07e6e3287588304fc9d391
rahmatsyifana12 <adean435@gmail.com>
2022-06-28 00:02:11 +0700

    Add login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const authService = require("../services/auth.service");
 
 async function register(req, res) {
     const { studentId, password } = req.body;
+
     try {
         await authService.register(studentId, password);
     } catch (error) {
@@ -14,6 +15,25 @@ async function register(req, res) {
     return res.status(201).json({
         status: 'success',
         message: 'Successfully registered an account'
+    });
+}
+
+async function login(req, res) {
+    const { studentId, password } = req.body;
+
+    try {
+
+    } catch (error) {
+        return res.status(error.statusCode).json({
+            status: 'fail',
+            message: error.message
+        });
+    }
+
+    return res.status(200).json({
+        status: 'success',
+        message: 'Successfully logged in',
+        data: {}
     });
 }
 
--- backend/src/validations/auth.validate.js
+++ backend/src/validations/auth.validate.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 const passwordSchema = joi.string()
     .min(6)
-    .max(64)
+    .max(32)
 
     .regex(/[0-9]/)
     .rule({ message: '{#label} requires at least a number' })
@@ -10,14 +10,14 @@ const passwordSchema = joi.string()
     .regex(/[a-z]/)
     .rule({ message: '{#label} requires at least a lowercase character' })
 
-    .regex(/[A-Z]/)
-    .rule({ message: '{#label} requires at least an uppercase character' });
-
 const newUserSchema = joi.object({
     studentId: joi.string()
         .min(5)
         .max(5)
+
         .regex(/^\d+$/)
+        .rule({ message: '{#label} can only be numeric' })
+
         .required(),
 
     password: passwordSchema.required()
