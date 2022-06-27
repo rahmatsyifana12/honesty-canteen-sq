90534364cf03b9432007b4f928d8c0f6bdc7a4be
rahmatsyifana12 <adean435@gmail.com>
2022-06-27 14:01:16 +0700

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { sequelize } = require('./models');
+const app = express();
+
+const port = 5000;
+app.use(express.json());
+
+app.listen({ port }, async () => {
+    await sequelize.authenticate();
+    console.log('Server is running at http://localhost:' + port);
+});
