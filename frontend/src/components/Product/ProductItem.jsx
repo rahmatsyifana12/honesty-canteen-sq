05d36f536bbb5ef636b4a7b62eea1d0c4702be22
rahmatsyifana12 <adean435@gmail.com>
2022-07-05 00:43:21 +0700

    Add buy product functionality

--- frontend/src/components/Product/ProductItem.jsx
+++ frontend/src/components/Product/ProductItem.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { PropTypes } from 'prop-types';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Card = styled.div`
     height: 500px;
@@ -9,12 +11,39 @@ const Card = styled.div`
 `;
 
 function ProductItem({ product }) {
+    const navigate = useNavigate();
+
+    const buy = async () => {
+        const accessToken = localStorage.getItem('accessToken');
+        try {
+            const response = await axios.delete(`http://localhost:5000/api/v1/products/buy/${product.id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            navigate('/products');
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data);
+            }
+        }
+    }
+
     return (
         <Card className='card m-2 p-2'>
             <div className="card-body text-center">
                 <h6 className="">{product.name}</h6>
                 <h5 className="mb-3">{product.price}</h5>
                 <p className="mb-3">{product.description}</p>
+
+                <button
+                    type="button"
+                    className="btn btn-primary btn-lg btn-block"
+                    style={{ fontSize: "16px" }}
+                    onClick={buy}
+                    >Buy
+                    </button>
             </div>
         </Card>
     );
