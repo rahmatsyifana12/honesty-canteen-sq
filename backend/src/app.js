d0afdc703ecded52aef8ec307a365dfd4414ac3b
rahmatsyifana12 <adean435@gmail.com>
2022-07-04 15:24:39 +0700

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const { sequelize } = require('./models');
 const config = require('./config/config');
@@ -7,6 +8,7 @@ const app = express();
 const PORT = config.port ?? 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, async () => {
